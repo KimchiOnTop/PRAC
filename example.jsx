d9bbe3a47264d1370fd58b21db450c3d0be0a72e
미니민지 <minjikim0632@gmail.com>
2025-08-22 15:33:09 +0900

    Create example.jsx

---
+++ example.jsx
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <h1>Hello, world!</h1>,
+    document.getElementById('root')
+);
+
+import ReactDOM from 'react-dom/client';
+
+createRoot(document.getElementById('root')).render(
+    <h1>Hello, world!</h1>
+);
